ba82c66f7b18161e2e8e36d2afddbae6c8c0306a
Jaron Li <2002jaron@gmail.com>
2023-12-05 18:19:30 -0800

    added ast types

--- ast.f90
+++ ast.f90
@@ -1,8 +1,40 @@
 module ast
     implicit none
 
-type :: NumC
+type :: ExprC
+end type ExprC
+
+type :: Symbol
+  character(:), allocatable :: symb
+end type Symbol
+
+
+type, extends(ExprC) :: NumC
   real :: n
-end type
+end type NumC
+
+
+type, extends (ExprC) :: StrC
+  character(:), allocatable :: s
+end type StrC
+
+type, extends (ExprC) :: AppC
+  type(ExprC) :: fun
+  type(ExprC), allocatable :: exprs
+end type AppC
+
+type, extends (ExprC) :: IfC
+  type(ExprC) :: expr, then, else
+end type IfC
+
+type, extends(ExprC) :: IdC
+  type(Symbol) :: n
+end type IdC
+
+type, extends (ExprC) :: LamC
+  type(Symbol), allocatable :: params
+  type(ExprC) :: body
+end type LamC
+
 
 end module ast
