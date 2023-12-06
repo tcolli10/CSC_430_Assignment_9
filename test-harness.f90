cbb5055b2b78e8869bb2da6282534d8c038f188f
Damon <damonlin916@gmail.com>
2023-12-06 09:48:47 -0800

    created assert functions for testing

---
+++ test-harness.f90
@@ -0,0 +1,36 @@
+module assert
+    implicit none
+    public assertNumber
+    public assertBoolean
+
+contains
+    function assertBoolean(expected, given) result(correct)
+        implicit none
+        logical, intent(in) :: expected
+        logical, intent(in) :: given
+        logical :: correct
+
+        if (expected .eqv. given) then
+            correct = .true.
+        else
+            correct = .false.
+        endif
+
+    end function assertBoolean
+
+    function assertNumber(expected, given) result(correct)
+        implicit none
+        integer, intent(in) :: expected
+        integer, intent(in) :: given
+        logical :: correct
+        
+        if (expected == given) then
+            correct = .true.
+        else
+            correct = .false.
+        endif
+    
+    end function assertNumber
+end module assert
+        
+
