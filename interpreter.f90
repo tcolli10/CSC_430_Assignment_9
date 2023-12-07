ce7791396de29019434242b59c39a8785ab9079c
Damon <damonlin916@gmail.com>
2023-12-07 09:57:13 -0800

    setting vals type in interp

--- interpreter.f90
+++ interpreter.f90
@@ -17,35 +17,45 @@ module interpreter
         type is (NumC)
             print *, "given NumC: ", expr%n
             val%n%n = expr%n
+            val%type = 'n'
         type is (StrC)
             print *, "given StrC: ", expr%s
             val%s%s = expr%s
+            val%type = 's'
         type is (IdC)
             print *, "given IdC: ", expr%n%symb
             symb = expr%n%symb
         type is (BoolC)
             if (expr%b == 1) then
                 val%b%bool = .true.
+                val%type = 'b'
             else
                 val%b%bool = .false.
+                val%type = 'b'
             endif
         type is (AppC)
             if (expr%fun == '+') then
                 ! write a getter to grab args from struct
                 val%n%n = expr%arg1%n + expr%arg2%n
+                val%type = 'n'
             else if (expr%fun == '-') then
                 val%n%n = expr%arg1%n - expr%arg2%n
+                val%type = 'n'
             else if (expr%fun == '*') then
                 val%n%n = expr%arg1%n * expr%arg2%n
+                val%type = 'n'
             else if (expr%fun == '/') then
                 val%n%n = expr%arg1%n / expr%arg2%n
+                val%type = 'n'
             endif
         type is (IfC)
             res = interp(expr%cond)
             if (res%b%bool .eqv. .true.) then
                 val = interp(expr%then)
+                val%type = 'b'
             else 
                 val = interp(expr%else)
+                val%type = 'b'
             endif
                     
 
